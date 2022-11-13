e6307883bed707b0060f2c5d6e6c180c06ddddd0
nero <ogudenero@gmail.com>
2022-11-13 16:00:19 +0000

    refacto

---
+++ PetstoreAutomation/src/test/java/uk/co/automation/petstore/AppTest.java
@@ -0,0 +1,20 @@
+package uk.co.automation.petstore;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}

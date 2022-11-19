1334423fc72999bb5f8ac3c0ca087805046f4a7b
nero <ogudenero@gmail.com>
2022-11-19 04:59:44 +0000

    refactor

---
+++ PetstoreAutomation/src/test/java/api/endPoint/userEndPoints.java
@@ -0,0 +1,48 @@
+package api.endPoint;
+
+import static io.restassured.RestAssured.*;
+import static io.restassured.matcher.RestAssuredMatchers.*;
+import static org.hamcrest.Matchers.*;
+
+import api.payload.User;
+import io.restassured.response.Response;
+
+public class userEndPoints {
+	
+	public static Response createUser(User payload) {
+		Response response=given()
+			.body(payload)
+			.contentType("application/json")
+			.accept("application/json")
+		
+		.when()
+			.post(Routes.post_url);
+		return response;
+	}
+	public static Response readUser(String userName){
+		Response response=given()
+			.pathParam("username", userName)
+		
+		.when()
+			.get(Routes.get_url);
+		return response;	
+	}
+	public static Response updateUser(String userName,User payload) {
+		Response response=given()
+			.pathParam("username",userName)
+			.body(payload)
+		
+		.when()
+			.put(Routes.update_url);
+		return response;
+	}
+	public static Response deleteUser(String userName){
+		Response response=given()
+			.pathParam("username", userName)
+		
+		.when()
+			.delete(Routes.delete_url);
+		return response;
+	}
+
+}

e69bf4288004988fdc8fcf0d21939afa78d7f593
nero <ogudenero@gmail.com>
2022-11-20 20:15:13 +0000

    fixes

--- livepersonalproject/src/main/java/base/WebDriverInstance.java
+++ livepersonalproject/src/main/java/base/WebDriverInstance.java
@@ -4,8 +4,12 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Properties;
+import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class WebDriverInstance {
 	
@@ -22,10 +26,29 @@ public class WebDriverInstance {
 		return driver.get();
 	}
 	public static WebDriver createDriver() throws IOException {
+		WebDriver driver=null;
 		Properties prop = new Properties();
 		FileInputStream data = new FileInputStream(System.getProperty("user.dir")+
 				"\\src\\main\\java\\resources\\config.properties" );
 		prop.load(data);
+		if(prop.getProperty("browser").equals("chrome")) {
+			System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+
+					"\\src\\main\\java\\drivers\\chromedriver.exe");
+			driver=new ChromeDriver();
+		}
+		else if(prop.getProperty("browser").equals("firefox")) {
+			System.setProperty("webdriver.gecko.driver", System.getProperty("user.dir")+
+					"\\src\\main\\java\\drivers\\geckodriver.exe");
+			driver=new FirefoxDriver();
+		}
+		else {
+			System.setProperty("webdriver.edge.driver", System.getProperty("user.dir")+
+					"\\src\\main\\java\\drivers\\msedgedriver.exe");
+			driver=new EdgeDriver();
+		}
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		return driver;
 	}
 
 }

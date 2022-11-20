9c0bc22db2056e507254d08323c2d486f3f5eff5
nero <ogudenero@gmail.com>
2022-11-20 20:07:40 +0000

    fixes

--- livepersonalproject/src/main/java/base/WebDriverInstance.java
+++ livepersonalproject/src/main/java/base/WebDriverInstance.java
@@ -1,6 +1,9 @@
 package base;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.Properties;
 
 import org.openqa.selenium.WebDriver;
 
@@ -16,7 +19,13 @@ public class WebDriverInstance {
 				e.printStackTrace();
 			}
 		}
+		return driver.get();
 	}
-	
+	public static WebDriver createDriver() throws IOException {
+		Properties prop = new Properties();
+		FileInputStream data = new FileInputStream(System.getProperty("user.dir")+
+				"\\src\\main\\java\\resources\\config.properties" );
+		prop.load(data);
+	}
 
 }

92d269958b7ea417049788a6b15c08f9f71b4929
nero <ogudenero@gmail.com>
2022-11-26 16:14:55 +0000

    fixes

--- MyFirstProject/src/main/java/pageObjects/HomePage.java
+++ MyFirstProject/src/main/java/pageObjects/HomePage.java
@@ -1,5 +1,44 @@
 package pageObjects;
 
-public class HomePage {
+import java.io.IOException;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import base.BasePage;
+
+public class HomePage extends BasePage{
+	
+	public WebDriver driver;
+	
+	By addressField= By.cssSelector("public WebDriver driver;");
+	By searchDropDown= By.cssSelector(".nav-search-scope");
+	By searchInputField= By.cssSelector("[name='field-keywords']");
+	By searchSubmitBtn= By.cssSelector("[value='Go']");
+	
+	public HomePage() throws IOException {
+		super();
+	}
+	public WebElement getAddressField() {
+		this.driver=getDriver();
+		return driver.findElement(addressField);	
+	}
+	public WebElement getSearchDropDown() {
+		this.driver=getDriver();
+		return driver.findElement(searchDropDown);
+	}
+	public WebElement getSearchInputField() {
+		this.driver=getDriver();
+		return driver.findElement(searchInputField);
+	}
+	public WebElement getSearchSubmitBtn() {
+		this.driver=getDriver();
+		return driver.findElement(searchSubmitBtn);
+	}
+
+	
+	
+	
 
 }
---
+++ MyFirstProject/src/main/java/pageObjects/ShopProductPage.java
@@ -0,0 +1,30 @@
+package pageObjects;
+
+import java.io.IOException;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import base.BasePage;
+
+public class ShopProductPage extends BasePage{
+	
+	public WebDriver driver;
+	
+	By prodOne = By.cssSelector("img[alt^='Sponsored Ad – ASUS Vivobook 15 M1502IA 15.6\" Full HD Laptop']");
+	By prodTwo = By.cssSelector("[srcset='https\\:\\/\\/m\\.media-amazon\\.com\\/images\\/I\\/71BxzF3S3CL\\._AC_UY218_\\.jpg 1x\\, https\\:\\/\\/m\\.media-amazon\\.com\\/images\\/I\\/71BxzF3S3CL\\._AC_UY327_FMwebp_QL65_\\.jpg 1\\.5x\\, https\\:\\/\\/m\\.media-amazon\\.com\\/images\\/I\\/71BxzF3S3CL\\._AC_UY436_FMwebp_QL65_\\.jpg 2x\\, https\\:\\/\\/m\\.media-amazon\\.com\\/images\\/I\\/71BxzF3S3CL\\._AC_UY545_FMwebp_QL65_\\.jpg 2\\.5x\\, https\\:\\/\\/m\\.media-amazon\\.com\\/images\\/I\\/71BxzF3S3CL\\._AC_UY654_FMwebp_QL65_\\.jpg 3x']");
+	
+	public ShopProductPage() throws IOException {
+		super();
+	}
+	public WebElement getProdOne() {
+		this.driver=getDriver();
+		return driver.findElement(prodOne);
+	}
+	public WebElement getProdTwo() {
+		this.driver=getDriver();
+		return driver.findElement(prodTwo);
+	}
+
+}

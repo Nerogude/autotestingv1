d512fc723e0d7751630b194db8a08137a483ee2c
nero <ogudenero@gmail.com>
2022-12-03 15:45:05 +0000

    fixes

--- livepersonalproject/src/main/java/base/BasePage.java
+++ livepersonalproject/src/main/java/base/BasePage.java
@@ -18,36 +18,37 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class BasePage {
 	
-	private String url;
 	private Properties prop;
-	public static String screenShotDestinationPath;
+	private String url;
+	public static String screenShotDestinationPath; 
 	
 	public BasePage() throws IOException {
 		
 		prop = new Properties();
 		FileInputStream data = new FileInputStream(System.getProperty("user.dir")+
 				"\\src\\main\\java\\resources\\config.properties");
-		prop.load(data);	
+		prop.load(data);
+		
 	}
 	public static WebDriver getDriver() {
 		return WebDriverInstance.getDriver();
 	}
 	public String getUrl() {
-		url=prop.getProperty("url");
+		url= prop.getProperty("url");
 		return url;
 	}
 	public static String takeSnapShot(String name) {
-		File srcFile = ((TakesScreenshot)getDriver()).getScreenshotAs(OutputType.FILE);
+		File srcFile= ((TakesScreenshot)getDriver()).getScreenshotAs(OutputType.FILE);
 		String destFile= System.getProperty("user.dir")+"\\target\\screenshot\\" + timestamp() + ".png";
-		screenShotDestinationPath=destFile;
+		screenShotDestinationPath= destFile;
 		try {
-			FileUtils.copyDirectory(srcFile, new File(destFile));
+			FileUtils.copyFile(srcFile, new File(destFile));
 		}catch (IOException e) {
 			e.printStackTrace();
 		}
-		return name;
+		return name;	
 	}
-	public static String timestamp() {return new SimpleDateFormat ("yyyy-MM-dd-HH-mm-ss").format(new Date());}
+	public static String timestamp() {return new SimpleDateFormat ("yyyy-MM-dd-HH-mm-ss").format (new Date());}
 	
 	public static String getScreenshotDestinationPath() {return screenShotDestinationPath;}
 	
@@ -56,7 +57,5 @@ public class BasePage {
 		wait.until(ExpectedConditions.invisibilityOf(element));
 	}
 
-	}
-	
 
-
+}
--- livepersonalproject/src/main/java/base/WebDriverInstance.java
+++ livepersonalproject/src/main/java/base/WebDriverInstance.java
@@ -1,7 +1,6 @@
 package base;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
@@ -13,7 +12,7 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class WebDriverInstance {
 	
-	public static ThreadLocal<WebDriver>driver= new ThreadLocal<>();
+	public static ThreadLocal<WebDriver>driver=new ThreadLocal<>();
 	
 	public static WebDriver getDriver() {
 		if(driver.get()==null) {
@@ -26,29 +25,32 @@ public class WebDriverInstance {
 		return driver.get();
 	}
 	public static WebDriver createDriver() throws IOException {
-		WebDriver driver=null;
+		WebDriver driver = null;
+		
 		Properties prop = new Properties();
 		FileInputStream data = new FileInputStream(System.getProperty("user.dir")+
-				"\\src\\main\\java\\resources\\config.properties" );
+				"\\src\\main\\java\\resources\\config.properties");
 		prop.load(data);
+		
 		if(prop.getProperty("browser").equals("chrome")) {
 			System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+
 					"\\src\\main\\java\\drivers\\chromedriver.exe");
-			driver=new ChromeDriver();
+			driver = new ChromeDriver();
 		}
 		else if(prop.getProperty("browser").equals("firefox")) {
 			System.setProperty("webdriver.gecko.driver", System.getProperty("user.dir")+
 					"\\src\\main\\java\\drivers\\geckodriver.exe");
-			driver=new FirefoxDriver();
+			driver = new FirefoxDriver();
 		}
 		else {
 			System.setProperty("webdriver.edge.driver", System.getProperty("user.dir")+
 					"\\src\\main\\java\\drivers\\msedgedriver.exe");
-			driver=new EdgeDriver();
+			driver = new EdgeDriver();
 		}
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		return driver;
+		
 	}
 	public static void cleanupDriver() {
 		driver.get().quit();

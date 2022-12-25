d91959186dcf7c75b31eb2705d08be53894f5d2f
nero <ogudenero@gmail.com>
2022-12-25 08:22:54 +0000

    fixes

--- universityproject/src/main/java/pageObjects/HomePage.java
+++ universityproject/src/main/java/pageObjects/HomePage.java
@@ -13,9 +13,28 @@ public class HomePage extends BasePage{
 	
 	By cypressWithCucumberLink = By.linkText("Cypress with Cucumber BDD - Beginner to Expert in 9 Hours!");
 	By seleniumWebdriverNewFeaturesLink = By.cssSelector("p:nth-of-type(2) > a");
-	By masteringSelectors = By.cssSelector("p:nth-of-type(2) > a");
-	By cypressv11 = By.cssSelector("p:nth-of-type(4) > a");
-	By seleniumWebdriverCucumber = By.cssSelector("p:nth-of-type(5) > a");
+	By masteringSelectorsLink = By.cssSelector("p:nth-of-type(2) > a");
+	By cypressv11Link = By.cssSelector("p:nth-of-type(4) > a");
+	By seleniumWebdriverCucumberLink = By.cssSelector("p:nth-of-type(5) > a");
+	By webdriverIoLink = By.cssSelector("p:nth-of-type(6) > a");
+	By contactUsLink = By.cssSelector("a#contact-us h1");
+	By loginPortalLink = By.cssSelector("a#login-portal h1");
+	By buttonClicksLink = By.cssSelector("a#button-clicks h1");
+	By toDoListLink = By.cssSelector("a#to-do-list h1");
+	By pageObjectModelLink = By.cssSelector("body [class='col-md-12']:nth-of-type(5) h1");
+	By accordionLink = By.cssSelector("div:nth-of-type(6) > a#page-object-model h1");
+	By dropdownCheckboxesLink = By.cssSelector("a#dropdown-checkboxes-radiobuttons h1");
+	By ajaxLoaderLink = By.cssSelector("a#ajax-loader h1");
+	By actionsLink = By.cssSelector("a#ajax-loader h1");
+	By scrollingAroundLink = By.cssSelector("a#ajax-loader h1");
+	By popupsLink = By.cssSelector("a#popup-alerts h1");
+	By iframeLink = By.cssSelector("a#iframe h1");
+	By hiddenElementsLink = By.cssSelector("a#hidden-elements h1");
+	By dataTablesLink = By.cssSelector("a#data-table h1");
+	By autocompleteTextfieldLink = By.cssSelector("a#autocomplete-textfield h1");
+	By fileUploadLink = By.cssSelector("a#file-upload h1");
+	By datepickerLink = By.cssSelector("a#datepicker h1");
+	By automationteststoreLink = By.cssSelector("[class='col-lg-12'] p");
 
 	public HomePage() throws IOException {
 		super();

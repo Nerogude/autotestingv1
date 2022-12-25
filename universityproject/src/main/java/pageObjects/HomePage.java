961133e31da8b03b9275bb3e104dc06a99d476e3
nero <ogudenero@gmail.com>
2022-12-25 07:50:40 +0000

    fixes

--- universityproject/src/main/java/pageObjects/HomePage.java
+++ universityproject/src/main/java/pageObjects/HomePage.java
@@ -2,6 +2,7 @@ package pageObjects;
 
 import java.io.IOException;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 import base.BasePage;
@@ -10,7 +11,11 @@ public class HomePage extends BasePage{
 	
 	public WebDriver driver;
 	
-	
+	By cypressWithCucumberLink = By.linkText("Cypress with Cucumber BDD - Beginner to Expert in 9 Hours!");
+	By seleniumWebdriverNewFeaturesLink = By.cssSelector("p:nth-of-type(2) > a");
+	By masteringSelectors = By.cssSelector("p:nth-of-type(2) > a");
+	By cypressv11 = By.cssSelector("p:nth-of-type(4) > a");
+	By seleniumWebdriverCucumber = By.cssSelector("p:nth-of-type(5) > a");
 
 	public HomePage() throws IOException {
 		super();

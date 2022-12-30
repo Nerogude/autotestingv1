dfb8815c67ceebc62227c7d66c64a7b1f5947176
nero <ogudenero@gmail.com>
2022-12-30 13:51:01 +0000

    fixes

--- universityproject/src/main/java/pageObjects/CartContentPanel.java
+++ universityproject/src/main/java/pageObjects/CartContentPanel.java
@@ -1,5 +1,32 @@
 package pageObjects;
 
-public class CartContentPanel {
+import java.io.IOException;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import base.BasePage;
+
+public class CartContentPanel extends BasePage{
+	
+	public WebDriver driver;
+	
+	By removeLink = By.cssSelector("div:nth-of-type(1) > div > ul:nth-of-type(1)  .shopping-item--container--20bda > .shopping-item--horizontal-actions--11OMZ > button:nth-of-type(1) > span");
+	By checkoutBtn = By.cssSelector(".checkout-button--checkout-button--1FJES.ud-btn.ud-btn-brand.ud-btn-large.ud-heading-md > span");
+	
+
+	public CartContentPanel() throws IOException {
+		super();
+	}
+	
+	public WebElement getRemoveLink() {
+		this.driver = getDriver();
+		return driver.findElement(removeLink);
+	}
+	public WebElement getCheckoutBtn() {
+		this.driver = getDriver();
+		return driver.findElement(checkoutBtn);
+	}
 
 }

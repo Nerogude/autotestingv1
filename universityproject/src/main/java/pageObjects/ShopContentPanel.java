03b288dee4916a0b15d0ca9f5ce9ddb4eba2274a
nero <ogudenero@gmail.com>
2022-12-30 13:41:46 +0000

    fixes

--- universityproject/src/main/java/pageObjects/ShopContentPanel.java
+++ universityproject/src/main/java/pageObjects/ShopContentPanel.java
@@ -1,5 +1,31 @@
 package pageObjects;
 
-public class ShopContentPanel {
+import java.io.IOException;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import base.BasePage;
+
+public class ShopContentPanel extends BasePage{
+	
+	public WebDriver driver;
+	
+	By goToCartBtn = By.cssSelector(".styles--go-to-cart--2K-KE.ud-btn.ud-btn-large.ud-btn-primary.ud-heading-md > span");
+	By addAllToCartBtn = By.cssSelector(".styles--cart-success-modal--GFoaO .bundle-unit--bundle-border--S8wVb .add-to-cart.bundle-unit--add-to-cart--3P_rg.ud-btn.ud-btn-brand.ud-btn-large.ud-heading-md");
+	
+
+	public ShopContentPanel() throws IOException {
+		super();
+	}
+	public WebElement getGoToCartBtn() {
+		this.driver = getDriver();
+		return driver.findElement(goToCartBtn);
+	}
+	public WebElement getAddAllToCartBtn() {
+		this.driver = getDriver();
+		return driver.findElement(addAllToCartBtn);
+	}
 
 }

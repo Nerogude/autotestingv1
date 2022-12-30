4812a842eaea5fb47d5c119d932394f45b8dbc0c
nero <ogudenero@gmail.com>
2022-12-30 13:36:11 +0000

    fixes

--- universityproject/src/main/java/pageObjects/CypressWithCucumberPage.java
+++ universityproject/src/main/java/pageObjects/CypressWithCucumberPage.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
+
 import base.BasePage;
 
 public class CypressWithCucumberPage extends BasePage{
